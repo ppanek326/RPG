6aff0b16fffe292aed7eab830bd85047c013006a
Patryk <panek2115@gmail.com>
2021-02-11 22:07:44 +0100

    dasd

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    
+
     Animator animator;
+    [SerializeField] GameObject[] skillParticles;
+    private IEnumerator coroutine;
+    [SerializeField] AudioClip[] skillsAudioClips;
+    [SerializeField] AudioSource audioSource;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-
+        for (int i = 0; i < skillParticles.Length; i++)
+        {
+            skillParticles[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -18,23 +25,52 @@ public class movement : MonoBehaviour
     {
         animator.SetFloat("vertical", Input.GetAxis("Vertical"));
         animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
-        if(Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKeyUp(KeyCode.Space))
         {
             Jump();
         }
         animator.SetBool("isrunning", true);
-        if(Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             run();
         }
+        if (Input.GetKeyUp(KeyCode.Alpha1)) UseSkill(1);
+        if (Input.GetKeyUp(KeyCode.Alpha2)) UseSkill(2);
+
+    }
+
+    public void UseSkill(int skillNumber)
+    {
+        animator.SetTrigger("Skill" + skillNumber);
+        skillParticles[skillNumber - 1].SetActive(true);
+        // audioSource.clip = skillsAudioClips[skillNumber - 1];
+        // audioSource.Play();
+        switch (skillNumber)
+        {
+            case 1:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 1);
+                break;
+            case 2:
+                coroutine = WaitToEnableObject(skillParticles[skillNumber - 1], 1);
+                break;
+        }
+
+
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator WaitToEnableObject(GameObject obj, float time)
+    {
+        yield return new WaitForSeconds(time);
+        obj.SetActive(false);
     }
 
     public void Jump()
     {
-        animator.SetTrigger("jump"); 
+        animator.SetTrigger("jump");
     }
     public void run()
     {
-        animator.SetBool("isrunning", false); 
+        animator.SetBool("run", false);
     }
 }
